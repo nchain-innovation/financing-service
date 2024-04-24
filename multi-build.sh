f1a18a1eb9d8dee3a75efb0ed0e59875b94ff5a9
Josie Wilden <j.wilden@nchain.com>
2024-04-24 12:21:45 +0100

    added multi-build script and bumped chain-gang version

---
+++ multi-build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This build file creates the rust financing service for use with the nchain rnd prototyping projects 
+
+# Project Id:  (financing-service)
+
+# Tags
+BASE_TAG=financing-service
+VERSION=v1.1
+PUBLISH_TAG=nchain/rnd-prototyping-$BASE_TAG:$VERSION
+
+# multi build, tag and push base images
+docker buildx build  --platform linux/amd64,linux/arm64 --push -t $PUBLISH_TAG .
