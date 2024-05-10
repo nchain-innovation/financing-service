14d12451d4889fa680b429dc579bb27bcb72387e
Josie Wilden <j.wilden@nchain.com>
2024-05-10 13:16:17 +0100

    Added curl to docker image so we can use the /status endpoint for health checks from docker compose

--- multi-build.sh
+++ multi-build.sh
@@ -6,8 +6,9 @@
 
 # Tags
 BASE_TAG=financing-service
-VERSION=v1.1
+VERSION=v1.2
 PUBLISH_TAG=nchain/rnd-prototyping-$BASE_TAG:$VERSION
 
 # multi build, tag and push base images
+# docker buildx build --builder cloud-nchain-rndprototyping --platform linux/amd64,linux/arm64 --push -t $PUBLISH_TAG .
 docker buildx build  --platform linux/amd64,linux/arm64 --push -t $PUBLISH_TAG .
